aacea5e2850db593ee92c21d6d4ef449a6b6faac
Sruti Jain <sruti.j1806@gmail.com>
2017-07-13 05:48:47 -0500

    Add files via upload

---
+++ src/Yelp-Reviews-to-MongoDB.py
@@ -0,0 +1,38 @@
+import os
+import time
+import json
+
+from pymongo import MongoClient
+from settings import Settings
+
+
+dataset_file = Settings.DATASET_FILE
+reviews_collection = MongoClient(Settings.MONGO_CONNECTION_STRING)[Settings.REVIEWS_DATABASE][
+    Settings.REVIEWS_COLLECTION]
+
+count = 0
+done = 0
+start = time.time()
+
+with open(dataset_file) as dataset:
+    count = sum(1 for line in dataset)
+
+with open(dataset_file) as dataset:
+    next(dataset)
+    for line in dataset:
+        try:
+            data = json.loads(line)
+        except ValueError:
+            print 'Oops!'
+        if data["type"] == "review":
+            reviews_collection.insert({
+                "reviewId": data["review_id"],
+                "business": data["business_id"],
+                "text": data["text"]
+            })
+
+        done += 1
+        if done % 100 == 0:
+            end = time.time()
+            os.system('cls')
+            print("Completed")
